0670db038a102199acfb909d51616509b1c0a5de
Grayson Carroll <carrollgt91@gmail.com>
2013-07-07 13:57:14 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PolianaApp::Application.config.secret_token = '596a58acfac46d61f9ed458e0438378d71c88f4eba7df10a2ac94f7ae871fb77033e6deac391f538db1f54ab98271d8471e6f6616ee0081fa5b3686531eaf889'

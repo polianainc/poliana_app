d5d5994a6e6458bead2b7ae8a85bf315c87328b3
Grayson Carroll <carrollgt91@gmail.com>
2013-09-12 20:25:45 -0500

    Patches the routes for production

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 PolianaApp::Application.routes.draw do
 
-  devise_for :users, :path_names => {:sign_in => "login", :sign_out => "logout"}
-
   get "static_pages/about"
   get "static_pages/policy"
   get "static_pages/terms"
   get "static_pages/landing"
   get "mailchimpSignup", to: "static_pages#mailchimpSignup"
 
-  get "bills/scrape", to: 'bills#scrape_congress'
   get "investors", to: 'static_pages#investors'
   get "demo", to: 'bills#index'
   get "bills/:id", to: 'bills#metadata'

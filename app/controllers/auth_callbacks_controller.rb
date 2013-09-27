3d99fafe99c58d8f167e308214ae591e7cd7016f
Grayson Carroll <carrollgt91@gmail.com>
2013-09-27 11:10:43 -0500

    Patches social sign on to include usernames and invite tokens

--- app/controllers/auth_callbacks_controller.rb
+++ app/controllers/auth_callbacks_controller.rb
@@ -3,7 +3,8 @@ class AuthCallbacksController < Devise::OmniauthCallbacksController
     def all
         user = User.from_omniauth(request.env['omniauth.auth'])
         user.invitation_key = session[:invitation_key]
-        if user.persisted?
+        binding.pry
+        if user.save
             sign_in_and_redirect user, notice: "Success!"
         else
             session["device.user_attributes"] = user.attributes

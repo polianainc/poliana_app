ab98987f0cbde0984b0aff19cb4aaedb2f246cea
Grayson Carroll <carrollgt91@gmail.com>
2013-11-20 12:06:23 -0600

    Patches the app configuration to log asset compilation

--- config/application.rb
+++ config/application.rb
@@ -67,6 +67,22 @@ module PolianaApp
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.assets.precompile << Proc.new do |path|
+     if path =~ /\.(css|js)\z/
+       full_path = Rails.application.assets.resolve(path).to_path
+       app_assets_path = Rails.root.join('app', 'assets').to_path
+       if full_path.starts_with? app_assets_path
+         puts "including asset: " + full_path
+         true
+       else
+         puts "excluding asset: " + full_path
+         false
+       end
+     else
+       false
+     end
+   end
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     

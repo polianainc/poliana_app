77892c08d89c13671e73a880d19ee47f470b8d3a
Grayson Carroll <carrollgt91@gmail.com>
2013-11-24 23:06:41 -0600

    Patches industry monthly total, adds stagingsmall configuration for emails

--- app/models/industry_monthly_total.rb
+++ app/models/industry_monthly_total.rb
@@ -1,4 +1,4 @@
 class IndustryMonthlyTotal
   include Mongoid::Document
-  store_in collection: "industry_monthly_totals
+  store_in collection: "industry_monthly_totals"
 end
--- config/environments/stagingsmall.rb
+++ config/environments/stagingsmall.rb
@@ -66,6 +66,7 @@ PolianaApp::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :amazon_ses
 
   # Enable threaded mode
   # config.threadsafe!

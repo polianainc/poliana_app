2b557e76e8d1443d3ce98c47b3dd66083999704b
Patrick Cason <me@patrickcason.com>
2014-01-08 16:40:01 -0600

    Finished implementing Jasmine and PhantomJS FRONT-21

--- spec/javascripts/static_pages_spec.js
+++
@@ -1,9 +0,0 @@
-//= require controllers/static_pages
-
-describe("Home page", function() {	
-	it("should play a video", function() {
-		var $videoLink = $('.play-video a');
-		
-		expect($videoLink).toExist();
-	});
-});

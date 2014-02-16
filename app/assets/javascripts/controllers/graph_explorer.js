31147386cca335ac3f39833d90422654d57835b7
Patrick Cason <me@patrickcason.com>
2014-02-16 14:36:28 -0600

    Implementing ajax loading of data, currently local

--- app/assets/javascripts/controllers/graph_explorer.js
+++ app/assets/javascripts/controllers/graph_explorer.js
@@ -29,6 +29,7 @@ ge = (function() {
 				for(var i = 0; i < _controller.graphs.length; i++) {
 					_controller.graphs[i].draw();
 				}
+				return this;
 			},
 			redraw: function() {
 				// Compute changes in data and draw incremental changes
@@ -66,65 +67,64 @@ ge = (function() {
 		_graph.unique_id = getUniqueID();
 		
 		_graph.draw = function() {
-			console.log($graph.text());
-			
 			var margin = {
 					top: _graph.margins.top,
 					bottom: _graph.margins.bottom,
 					left: _graph.margins.left,
 					right: _graph.margins.right
 				},
-			    width = _graph.width - margin.left - margin.right,
-			    height = _graph.height - margin.top - margin.bottom;
+				width = _graph.width - margin.left - margin.right,
+				height = _graph.height - margin.top - margin.bottom;
 			
 			var x = d3.scale.ordinal()
-			    .rangeRoundBands([0, width], .1);
+				.rangeRoundBands([0, width], .1);
 
 			var y = d3.scale.linear()
-			    .range([height, 0]);
+				.range([height, 0]);
 
 			var xAxis = d3.svg.axis()
-			    .scale(x)
-			    .orient("bottom");
+				.scale(x)
+				.orient("bottom");
 
 			var yAxis = d3.svg.axis()
-			    .scale(y)
-			    .orient("left")
-			    .ticks(10, "%");
+				.scale(y)
+				.orient("left")
+				.ticks(10, "%");
 
-			var svg = d3.select("body").append("svg")
-			    .attr("width", width + margin.left + margin.right)
-			    .attr("height", height + margin.top + margin.bottom)
-			  .append("g")
-			    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+			var svg = d3.select($graph.selector).append("svg")
+				.attr("width", width + margin.left + margin.right)
+				.attr("height", height + margin.top + margin.bottom)
+				.style("background-color", "black")
+				.append("g")
+					.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 			/*
 			d3.tsv("data.tsv", type, function(error, data) {
 			  x.domain(data.map(function(d) { return d.letter; }));
 			  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
 
 			  svg.append("g")
-			      .attr("class", "x axis")
-			      .attr("transform", "translate(0," + height + ")")
-			      .call(xAxis);
+				  .attr("class", "x axis")
+				  .attr("transform", "translate(0," + height + ")")
+				  .call(xAxis);
 
 			  svg.append("g")
-			      .attr("class", "y axis")
-			      .call(yAxis)
-			    .append("text")
-			      .attr("transform", "rotate(-90)")
-			      .attr("y", 6)
-			      .attr("dy", ".71em")
-			      .style("text-anchor", "end")
-			      .text("Frequency");
+				  .attr("class", "y axis")
+				  .call(yAxis)
+				.append("text")
+				  .attr("transform", "rotate(-90)")
+				  .attr("y", 6)
+				  .attr("dy", ".71em")
+				  .style("text-anchor", "end")
+				  .text("Frequency");
 
 			  svg.selectAll(".bar")
-			      .data(data)
-			    .enter().append("rect")
-			      .attr("class", "bar")
-			      .attr("x", function(d) { return x(d.letter); })
-			      .attr("width", x.rangeBand())
-			      .attr("y", function(d) { return y(d.frequency); })
-			      .attr("height", function(d) { return height - y(d.frequency); });
+				  .data(data)
+				.enter().append("rect")
+				  .attr("class", "bar")
+				  .attr("x", function(d) { return x(d.letter); })
+				  .attr("width", x.rangeBand())
+				  .attr("y", function(d) { return y(d.frequency); })
+				  .attr("height", function(d) { return height - y(d.frequency); });
 
 			});
 
--- app/assets/javascripts/controllers/politicians.js
+++ app/assets/javascripts/controllers/politicians.js
@@ -1,29 +1,47 @@
+// Get our biogiude
+var bioguide = $('#primary-key').attr('data-id');
+
+// Define our controller
 var cont = ge.controller();
 
-var pacsBar = ge.verticalBarGraph({
-	width: 400,
-	height: 400,
-	selector: $('#pacs-bar'),
-	margins: {
-		all: 40
-	}
+// Get all the PACS
+//var getPacs = $.get('//default-environment-ygymzummgf.elasticbeanstalk.com/politicians/' + bioguide + '/contributions/pacs', function(data) {
+var getPacs = $.get('/temp/pacs.json', function(data) {
+	var pacsBar = ge.verticalBarGraph({
+		width: 400,
+		height: 400,
+		selector: $('#pacs-bar'),
+		margins: {
+			all: 40
+		}
+	});
+	
+	cont.addGraph(pacsBar);
 });
 
-var industriesBar = ge.verticalBarGraph({
-	width: 400,
-	height: 400,
-	selector: $('#industries-bar'),
-	margins: {
-		top: 40,
-		bottom: 30,
-		left: 20,
-		right: 10
-	}
+// Get all the industries
+//var getIndustries = $.get('//default-environment-ygymzummgf.elasticbeanstalk.com/politicians/' + bioguide + '/contributions/industries', function(data) {
+var getIndustries = $.get('/temp/industries.json', function(data) {
+	var industriesBar = ge.verticalBarGraph({
+		width: 400,
+		height: 400,
+		selector: $('#industries-bar'),
+		margins: {
+			top: 40,
+			bottom: 30,
+			left: 20,
+			right: 10
+		}
+	});
+	
+	cont.addGraph(industriesBar);
 });
 
-cont.addGraph(pacsBar)
-	.addGraph(industriesBar);
+// Tell jQuery's AJAX to be patient
+$.when(getPacs, getIndustries).done(function() {
+	// Let's rock and roll
+	cont.render();
 
-console.log(cont.listGraphs());
-
-cont.render();
+	// Show me what ya got
+	console.log(cont.listGraphs());
+});

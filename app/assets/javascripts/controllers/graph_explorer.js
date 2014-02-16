d8fd62ee4c378e5a815cc2c3f77f959f0400de50
Patrick Cason <me@patrickcason.com>
2014-02-15 18:01:39 -0600

    Initial graph explorer facade and module pattern POL-253

--- app/assets/javascripts/controllers/graph_explorer.js
+++ app/assets/javascripts/controllers/graph_explorer.js
@@ -1,31 +1,56 @@
 ge = (function() {
+	"use strict"
+	
 	var ge = {
 		version: '1.0'
 	};
 	
 	ge.controller = function(_controller) {
 		if(_controller === undefined)
-			_controller = {};
+			var _controller = {};
 			
-		var _graphs = {};
-		
-		_controller.addGraph = function(graph) {
-			_graphs[graph.name] = graph;
-		};
-		
+		_controller.graphs = [];
+			
 		return {
-			add: function(graph) {
-				_controller.addGraph(graph);
+			addGraph: function(graph) {
+				_controller.graphs.push(graph.settings());
 				return this;
 			},
-			list: function() {
-				return _graphs;
+			getGraph: function(index) {
+				return _controller.graphs[index];
+			},
+			listGraphs: function() {
+				return _controller.graphs;
+			},
+			settings: function() {
+				return _controller;
 			}
 		};
 	};
 	
-	ge.verticalBarGraph = function(_chart) {
-		return _chart;
+	ge.verticalBarGraph = function(_graph) {
+		if(_graph === undefined)
+			var _graph = {};
+			
+		_graph.unique_id = getUniqueID();
+		
+		function getUniqueID() {
+			return Math.random().toString(36).substr(2, 9);
+		}
+		
+		return {
+			width: function(width) {
+				_graph.width = width;
+				return this;
+			},
+			height: function(height) {
+				_graph.height = height;
+				return this;
+			},
+			settings: function() {
+				return _graph;
+			}
+		};
 	};
 	
 	return ge;
--- app/assets/javascripts/controllers/politicians.js
+++ app/assets/javascripts/controllers/politicians.js
@@ -1,18 +1,16 @@
 var cont = ge.controller({
-	name: "dat-controller"
+	name: "controller"
 });
 
-var bar1 = ge.verticalBarGraph({
-	name: "my-bar",
-	width: 100,
-	height: 200
-});
+var bar1 = ge.verticalBarGraph();
 
 var bar2 = ge.verticalBarGraph({
-	name: "your-bar",
-	width: 300,
-	height: 100
-});
+	width: 500,
+	height: 300
+}).width(100);
 
-cont.add(bar1).add(bar2);
-console.log(cont.list());
+cont.addGraph(bar1).addGraph(bar2);
+
+bar1.width(400).height(200);
+
+console.log(cont.listGraphs());

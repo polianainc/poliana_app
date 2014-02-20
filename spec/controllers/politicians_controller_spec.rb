84443ebeb9ad3de147f28d17b145b8c67623392f
Grayson Carroll <carrollgt91@gmail.com>
2014-02-20 13:41:48 -0600

    Adds some basic testing to Search and Politician controllers, updates the factories so we have a basic politician model in there, includes some helpers to allow us to test sunspot, and patches an error where the politician_controller was using an incorrect query to fetch all politicians

--- spec/controllers/politicians_controller_spec.rb
+++ spec/controllers/politicians_controller_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper'
 
 describe PoliticiansController do
   it "should render a list of politicians in json" do
-    
+    request.env['HTTP_ACCEPT'] = 'application/json'
+    get 'index'
+    JSON.parse(response.body)
+  end
+
+  it "should render a valid politician's individual page" do
+    get 'show', :id => "O000167"
+    response.should render_template :show
   end
 end

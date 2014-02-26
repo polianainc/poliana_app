e22f9807a301c5248f436dc12c175cf2f7762d3d
Grayson Carroll <carrollgt91@gmail.com>
2014-02-26 14:06:39 -0600

    Patches the asset precompilation settings to be the default

--- config/environments/stagingsmall.rb
+++ config/environments/stagingsmall.rb
@@ -17,22 +17,6 @@ PolianaApp::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
 
-  config.assets.precompile << Proc.new do |path|
-     if path =~ /\.(css|js)\z/
-       full_path = Rails.application.assets.resolve(path).to_path
-       app_assets_path = Rails.root.join('app', 'assets').to_path
-       if full_path.starts_with? app_assets_path
-         puts "including asset: " + full_path
-         true
-       else
-         puts "excluding asset: " + full_path
-         false
-       end
-     else
-       false
-     end
-   end
-
   # Generate digests for assets URLs
   config.assets.digest = true
 

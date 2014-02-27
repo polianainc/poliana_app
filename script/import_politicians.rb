fb4d223e9839b4f76045ce7eec73d62833760d50
Grayson Carroll <carrollgt91@gmail.com>
2014-02-27 17:43:08 -0600

    Patches the import script to correct term end timestamps being 0, adds some hackery to prevent congress 114 to be included in the import, adds pre-election term calculation to the import script so that the rails metadata indicates there should be campaign finance information during those pre election terms

--- script/import_politicians.rb
+++ script/import_politicians.rb
@@ -1,19 +1,40 @@
+@start_date = Date.new(2003, 1, 1)
+
 def import_raw_legislators_to_mongo
   
   json = File.read('lib/assets/legislators.json')
   legislators = JSON.parse(json)
-  start_date = Date.new(2003, 1, 1)
-
+  
   legislators.each do |pol|
 
     if pol["bioguide_id"] == nil
-      puts pol["first_name"] + " " + pol["last_name"] 
+      puts "bioguide nil: " + pol["first_name"] + " " + pol["last_name"] 
       next
     end
 
     term_start = DateTime.strptime((pol["begin_timestamp"]).to_s, '%s').to_date
-    if term_start < start_date
+    term_end = DateTime.strptime((pol["end_timestamp"]).to_s, '%s').to_date
+
+    if term_start < @start_date
       next
+    end
+
+    # incorrect term end senate fix
+    if term_end < @start_date && pol["term_type"] == "sen" && term_start + 6.years <= Date.today
+      puts "incorrect term end senate: " + pol["first_name"] + " " + pol["last_name"] + ", end: " + term_end.to_s
+      term_end = term_start + 6.years
+    end
+
+    # incorrect term end house fix
+    if term_end < @start_date && pol["term_type"] == "rep" && term_start + 2.years <= Date.today
+      puts "incorrect term end house: " + pol["first_name"] + " " + pol["last_name"] + ", end: " + term_end.to_s
+      term_end = term_start + 2.years
+    end
+
+    # elected within term limit fix
+    if term_end < @start_date
+      puts "elected within term limit: " + pol["first_name"] + " " + pol["last_name"] + ", elected on: " + term_start.to_s
+      term_end = Date.today
     end
 
     mpol = Politician.where(:bioguide_id => pol["bioguide_id"])[0]
@@ -37,12 +58,16 @@ def import_raw_legislators_to_mongo
     term = Term.new()
     term.term_type = pol["term_type"]
     term.start = term_start
-    term.end = DateTime.strptime((pol["end_timestamp"]).to_s, '%s').to_date
+    term.end = term_end
 
     mpol.terms << term
 
     mpol.save()
   end
+
+  add_birthday_stats
+  add_pre_election_terms
+  add_congresses
 end
 
 def add_congresses
@@ -55,7 +80,9 @@ def add_congresses
       i = term.start
 
       while(i < (term.end - 1.month))
-        term.congresses << (i.year - 1787) / 2 
+        cong_num = (i.year - 1787) / 2
+        # HACK: Remove 114 congress since we don't have data on that
+        term.congresses <<  cong_num if (cong_num != 114)
         i += 1.year
       end
 
@@ -76,6 +103,28 @@ def add_birthday_stats
     puts pol.percent_age_difference
   end
 end
-import_raw_legislators_to_mongo
-add_congresses
-add_birthday_stats
+
+def add_pre_election_terms
+  Politician.all.each do |pol|
+
+    first_term = pol.terms.sort_by { |t| t.start }[0]
+
+    pre_election_term_start = first_term.start - 2.years - 1.day
+
+    if pre_election_term_start < @start_date
+      next
+    else
+      pre_election_term = Term.new()
+
+      pre_election_term.start = pre_election_term_start
+      pre_election_term.end = pre_election_term_start + 2.years
+
+      pol.terms << pre_election_term
+      pol.save()
+    end
+  end
+end
+
+# RUN THE CODE
+
+import_raw_legislators_to_mongo()

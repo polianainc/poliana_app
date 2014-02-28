97f8b49b2dcc1c6cc891be2364f67ee37e8b4f2d
Patrick Cason <me@patrickcason.com>
2014-02-28 12:28:48 -0600

    Added state helper again

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -76,24 +76,4 @@ class String
     string_arr = self.split(' ')
     string_arr.count > limit ? "#{string_arr[0..(limit-1)].join(' ')}..." : self
   end
-  
-  def convert_party(to)
-    if to == "abbrev"
-  		if self == "Democrat" or self == "Democrats"
-  			"D"
-  		elsif self == "Republican" or self == "Republicans"
-  			"R"
-  		else
-  			"I"
-			end
-  	else
-  		if self == "D"
-  			"Democrat"
-  		elsif self == "R"
-  			"Republican"
-  		else	
-  			"Independent"
-			end
-  	end
-  end
 end
